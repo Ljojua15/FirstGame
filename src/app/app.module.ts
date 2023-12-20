f29abd9e1ad8287d3f1d4421ce44db9afcbbac61
Ljojua15 <jojualasha15@gmail.com>
2023-12-20 13:06:27 +0400

    'test'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IframeComponent } from './iframe/iframe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IframeComponent
   ],
   imports: [
     BrowserModule,
